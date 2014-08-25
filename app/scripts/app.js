3cdfb1b2e9ab976660d80f6e62b9fdc145cb32be
Jian Xiong <a0097815@nus.edu.sg>
2014-08-25 17:38:49 +0800

    captcha working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,45 @@
 'use strict';
 
 var app = angular
-  .module('afterYouEmsApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'reCAPTCHA'
+    .module('afterYouEmsApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'reCAPTCHA',
+        'ui.router'
+    ])
 
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
+
+/**.config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+        $stateProvider
+            .state('main', {
+                url: "/",
+                templateUrl: "view/main.html",
+                controller: "MainCtrl"
+            })
+            .state('login', {
+                url: "/login",
+                templateUrl: "view/login.html",
+                controller: "LoginCtrl"
+            })
+    });**/
+
+
--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,37 +1,22 @@
 'use strict';
 
 angular.module('afterYouEmsApp')
-    .controller('LoginCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+    .config(function (reCAPTCHAProvider) {
 
-        $scope.model = {
-            key: '6LddIvkSAAAAAJBd0N6rx42k5YoTZjq_dOHnJNQw'
-        };
+        reCAPTCHAProvider.setPublicKey('6LddIvkSAAAAAJBd0N6rx42k5YoTZjq_dOHnJNQw');
 
-        $scope.submit = function () {
-            var valid;
+        reCAPTCHAProvider.setOptions({
+            theme: 'clean'
+        });
+    })
 
-            /**
-             * SERVER SIDE VALIDATION
-             *
-             * You need to implement your server side validation here.
-             * Send the model.captcha object to the server and use some of the server side APIs to validate it
-             * See https://developers.google.com/recaptcha/docs/
-             */
-            console.log('sending the captcha response to the server', vcRecaptchaService.data());
-
-            if (valid) {
-                console.log('Success');
-
-            } else {
-                console.log('Failed validation');
-
-                // In case of a failed validation you need to reload the captcha because each challenge can be checked just once
-                vcRecaptchaService.reload();
+    .controller('LoginCtrl', function ($scope, reCAPTCHA) {
+        $scope.email = "";
+        $scope.password = "";
+        $scope.register = function () {
+            if ($scope.registerForm.$valid) {
+                $scope.showdialog = true;
+                console.log('Form is valid');
             }
-        };
+        }
     });
